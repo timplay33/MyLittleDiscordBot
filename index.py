690ded198bd789321876b0c3996dcb29267d229c
Timplay33 <tim@heidler.eu.org>
2023-09-02 00:23:43 +0200

    added sync command

--- index.py
+++ index.py
@@ -23,8 +23,20 @@ tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=GUILD_ID))
-    print("Ready!")
+    # await tree.sync(guild=discord.Object(id=GUILD_ID))
+    print(f'We have logged in as {client.user}')
+
+
+@tree.command(name="sync", description="syncs commands to the server")
+async def sync(interaction: discord.Interaction):
+    if interaction.user == interaction.user:  # ToDo - check if the user is the owner of the server
+        try:
+            await tree.sync(guild=discord.Object(id=interaction.guild_id))
+            await interaction.response.send_message(f"Syncing commands to the server (guild_id = {interaction.guild_id})", ephemeral=True)
+        except:
+            await interaction.response.send_message(f"Failed to sync commands to the server (guild_id = {interaction.guild_id})", ephemeral=True)
+    else:
+        await interaction.response.send_message(f"you don't have permission to sync commands to the server", ephemeral=True)
 
 # /help - Help menu
 
@@ -38,14 +50,14 @@ async def help(interaction: discord.Interaction):
 
 @tree.command(name="hello", description="Tells you that you used a slash command")
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hey {interaction.user.mention}! This is a slash command!", ephemeral=True)
+    await interaction.response.send_message(f"Hey {interaction.user.display_name}! This is a slash command!", ephemeral=True)
 
 # /say
 
 
 @tree.command(name="say", description="says what you give it")
 async def say(interaction: discord.Interaction, arg: str):
-    await interaction.response.send_message(f"{interaction.user.name} said: {arg}!")
+    await interaction.response.send_message(f"{interaction.user.display_name} said: {arg}!")
 
 # /serverinfo
 
@@ -62,5 +74,13 @@ async def serverinfo(interaction: discord.Interaction):
     embed.add_field(name="Server creation date",
                     value=interaction.guild.created_at.date(), inline=False)
     await interaction.response.send_message(embed=embed, ephemeral=True)
+
+
+@tree.command(name="getrole", description="gives a role to a user")
+async def getrole(interaction: discord.Interaction):
+    try:
+        await interaction.response.send_message(f"{interaction.user.name} gave you a role!", ephemeral=True)
+    except:
+        await interaction.response.send_message(f"{interaction.user.name} did not give you a role!", ephemeral=True)
 
 client.run(DISCORD_TOKEN)
